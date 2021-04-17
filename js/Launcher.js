9692f1ac57a6d04a2cd1e831f59ef73747f55b34
jeopardyKing <bhararaaayush@gmail.com>
2021-04-17 18:16:24 +0530

    1

---
+++ js/Launcher.js
@@ -0,0 +1,39 @@
+class Launcher { 
+    constructor(body,anchor){
+        
+        var options = {
+            bodyA : body,
+            pointB : anchor,
+            stiffness : 0.009,
+            length : 4
+        }
+
+        this.bodyA = body;
+        this.pointB = anchor ;
+
+        this.launcher = Constraint.create(options);
+        World.add(world,this.launcher);
+    }
+
+    attach(body){
+        this.launcher.bodyA = body;
+    }
+
+    fly(){
+        this.launcher.bodyA = null;
+    }
+
+    display(){
+
+        if(this.launcher.bodyA){
+
+        var pointA = this.bodyA.position;
+        var pointB = this.pointB;
+        
+        strokeWeight(1);
+        stroke("red");
+        line(pointA.x,pointA.y,pointB.x,pointB.y);
+
+        }
+    }
+}
